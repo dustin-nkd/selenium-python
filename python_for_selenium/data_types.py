a585d7b4b176bfc99f9c27d0391cb468e7ba5c20
Nguyen Khanh Duy <nguyenkhanhduy.sgd@gmail.com>
2025-02-11 14:26:01 +0700

    refactor folder

---
+++ python_for_selenium/data_types.py
@@ -0,0 +1,9 @@
+int_number = 8386
+float_number = 10.10
+full_name = "Nguyen Khanh Duy"
+is_boolean = True
+
+print(type(int_number))
+print(type(float_number))
+print(type(full_name))
+print(type(is_boolean))
