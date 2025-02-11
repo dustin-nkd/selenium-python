5191324c7f6ddd15937d62cd28131d0fde0f9bd1
Nguyen Khanh Duy <nguyenkhanhduy.sgd@gmail.com>
2025-02-11 11:27:29 +0700

    first commit

---
+++ print_statements.py
@@ -0,0 +1,3 @@
+print("Hi i am Nguyen Khanh Duy", end="")
+print("I'm 26 years old", end="")
+print(8386)
---
+++ variables.py
@@ -0,0 +1,6 @@
+a, b, c = 1, 2, 3
+my_first_name = "Nguyen"
+
+print(a, b, c)
+print(my_first_name)
+print("This is my first name: ", my_first_name)
