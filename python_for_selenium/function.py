494ab5e939539877372b1780dfdf946bfcec734d
Nguyen Khanh Duy <nguyenkhanhduy.sgd@gmail.com>
2025-02-13 14:42:34 +0700

    function

---
+++ python_for_selenium/function.py
@@ -0,0 +1,13 @@
+def print_full_name(full_name="Nguyen Khanh Duy"):
+    print("My full name is: ", full_name)
+
+
+print_full_name("Dustin On The Go")
+
+
+def add(first, second):
+    return first + second
+
+
+result = add(10, 113)
+print(result)
