7b8d9bc160df7d5c5b2ac7cf1aa5ee6ad1b6676f
Nguyen Khanh Duy <nguyenkhanhduy.sgd@gmail.com>
2025-05-13 11:35:56 +0700

    rework handle calendar type 1

--- selenium_webdriver/handle_calendar_type_1.py
+++ selenium_webdriver/handle_calendar_type_1.py
@@ -15,7 +15,7 @@ driver.find_element(By.XPATH, "//input[@id='datepicker']").click()
 wait.until(expected_conditions.visibility_of_element_located((By.XPATH, "//div[@id='ui-datepicker-div']")))
 
 
-def select_day_in_calender(day, month, year):
+def select_future_day_in_calender(day, month, year):
     current_month = driver.find_element(By.XPATH, "//span[@class='ui-datepicker-month']").text
     current_year = driver.find_element(By.XPATH, "//span[@class='ui-datepicker-year']").text
 
@@ -28,5 +28,18 @@ def select_day_in_calender(day, month, year):
     time.sleep(3)
 
 
-select_day_in_calender(31, "December", "2030")
+def select_past_day_in_calender(day, month, year):
+    current_month = driver.find_element(By.XPATH, "//span[@class='ui-datepicker-month']").text
+    current_year = driver.find_element(By.XPATH, "//span[@class='ui-datepicker-year']").text
+
+    while not (current_month == month and current_year == year):
+        driver.find_element(By.XPATH, "//span[text()='Prev']").click()
+        current_month = driver.find_element(By.XPATH, "//span[@class='ui-datepicker-month']").text
+        current_year = driver.find_element(By.XPATH, "//span[@class='ui-datepicker-year']").text
+
+    driver.find_element(By.XPATH, f"//td[@data-handler='selectDay']//a[text()='{day}']").click()
+    time.sleep(3)
+
+
+select_past_day_in_calender(10, "May", "2021")
 driver.quit()
